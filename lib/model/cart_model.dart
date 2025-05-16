2ad552226a80aa405ea4b19de6df953d2afa13a7
Tasnim Aj <tasnimajaj1234@gmail.com>
2025-05-16 21:04:15 +0300

    clock_page

--- lib/model/cart_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:state_managment/model/product_model.dart';
-
-class CartModel {
-  List<ProductModel> products;
-  double totalPrice = 0;
-
-  CartModel({required this.products});
-}
--- lib/model/product_model.dart
+++
@@ -1,7 +0,0 @@
-class ProductModel {
-  int id;
-  String name;
-  double price;
-  int quantity = 1;
-  ProductModel({required this.id, required this.name, required this.price});
-}
